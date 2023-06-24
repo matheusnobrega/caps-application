80e22dd714724d8843bd7e370aaf554576f70f15
matheusnobrega <matheus.nobrega.duarte@gmail.com>
2023-06-24 02:51:02 -0300

    Cadastro de consumo feito

--- paciente/views.py
+++ paciente/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Paciente, Droga
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Paciente, Droga, DrogaPaciente
 
 def cadastrar_paciente(request):
     if request.method == 'POST':
@@ -34,12 +34,30 @@ def listar_pacientes(request):
 
 def detalhe_paciente(request, pk):
     paciente = get_object_or_404(Paciente, pk=pk)
+    drogas = DrogaPaciente.objects.filter(paciente__id=pk)
 
     return render(request, 'detalhe_paciente.html', {
-        'paciente': paciente
+        'paciente': paciente,
+        'drogas': drogas,
     })
 
 def adiciona_droga(request, pk):
+    if request.method == 'POST':
+        droga_id = request.POST['droga']
+        idade_pu = request.POST['idade']
+        frequencia = request.POST['frequencia']
+        ultimo_uso = request.POST['ultimo-uso']
+
+        droga = Droga.objects.get(pk=droga_id)
+        paciente = Paciente.objects.get(pk=pk)
+
+        droga_paciente = DrogaPaciente(droga=droga, paciente=paciente, idade_pu=idade_pu,
+                                       frequencia=frequencia, ultimo_uso=ultimo_uso)
+        droga_paciente.save()
+
+        return redirect('paciente:detalhe_paciente', pk)
+
+
     drogas = Droga.objects.all()
 
     return render(request, 'adiciona_droga.html', {
